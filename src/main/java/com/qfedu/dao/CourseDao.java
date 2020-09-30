dc3744c7f0f2eb008aa2755e4100a18876ea7318
sun <1476512306@qq.com>
2020-09-30 11:25:02 +0800

    科学管理数据修改已完成

--- src/main/java/com/qfedu/dao/CourseDao.java
+++ src/main/java/com/qfedu/dao/CourseDao.java
@@ -8,4 +8,5 @@ public interface CourseDao {
     public void insertCourse(Course course);
     public List<Course> selectCourseAll();
     public void deleteCourseById(int id);
+    public void updateCourse(Course course);
 }
