e8e35eb2e0b47694f1bad108cb0f7be7528f8f37
sun <1476512306@qq.com>
2020-09-30 10:43:40 +0800

    科学管理科学展示已完成

--- src/main/java/com/qfedu/dao/CourseDao.java
+++ src/main/java/com/qfedu/dao/CourseDao.java
@@ -2,6 +2,9 @@ package com.qfedu.dao;
 
 import com.qfedu.entity.Course;
 
+import java.util.List;
+
 public interface CourseDao {
     public void insertCourse(Course course);
+    public List<Course> selectCourseAll();
 }
